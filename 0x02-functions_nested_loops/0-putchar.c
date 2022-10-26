7b8ef327637f416ca92080d4bddaca031848d0d3
Bukolaambrose <roselynambrose1@gmail.com>
2022-10-26 05:46:09 -0700

    oct 21

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * main - print _putchar
+ * main - prints _putchar and a new line.
  * is written by bukola
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
